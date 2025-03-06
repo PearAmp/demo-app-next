24397467d57ffaedba60aaf25956594702771374
Nu Chaimongkon <nu.chaimongkon@devmaewmaew.io>
2025-03-06 14:47:01 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,6 +13,9 @@ export default function Home() {
           height={38}
           priority
         />
+        <div>
+          <Link href={"/pages/about"}>About</Link>
+        </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
