281c1598c3cffbecd838e565d57910263083ab0c
Nu Chaimongkon <nu.chaimongkon@devmaewmaew.io>
2025-09-24 13:25:02 +0700

    add drawer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "./components/Navbar/navbar";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="page">
-      <h1>54564545</h1>
+    <div className="homepage">
+      <div className="bg-home">
+        <Image
+          src="/images/bg/bg-home.png"
+          alt="Logo"
+          width={1970} // set your image width
+          height={480}
+        />
+      </div>
+     <div className="item-custom">
+       <div className="grid grid-cols-12 ">
+        <div className="col-span-4 ">
+          <h2>ฟิลเตอร์</h2>
+          <h3>ราคา/แผ่น</h3>
+        </div>
+        <div className="col-span-8 ">Col 8</div>
+      </div>
+     </div>
     </div>
   );
 }
